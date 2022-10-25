2759ff6f6603c554a5047a9aeeb2c0d462c9cb4e
cste14 <emonnstu14@gmail.com>
2022-10-26 00:37:03 +0600

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./layout/Main";
+import Login from "./component/auth/Login";
+import Register from "./component/auth/Register";
+import Course from "./screen/course/Course";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/course",
+        element: <Course />,
+      },
+      {
+        path: "/user/login",
+        element: <Login />,
+      },
+      {
+        path: "/user/login",
+        element: <Login />,
+      },
+      {
+        path: "/user/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
 function App() {
-  return (
-    <div>
-      <h1>Client</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
