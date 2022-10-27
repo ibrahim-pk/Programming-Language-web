957883a1cfac629b824a8480b66a03da44a0f229
cste14 <emonnstu14@gmail.com>
2022-10-27 13:37:25 +0600

    header

---
+++ src/component/header/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="aboutProgramming container">
+      <div>
+        <h3 className="my-2">What is a Programming Language?</h3>
+        <h6>
+          A programming language is a computer language that is used by
+          programmers (developers) to communicate with computers. It is a set of
+          instructions written in any specific language ( C, C++, Java, Python)
+          to perform a specific task
+        </h6>
+      </div>
+      <div className="headerImg">
+        <img
+          className="img-fluid"
+          src="https://assets-api.kathmandupost.com/thumb.php?src=https://assets-cdn.kathmandupost.com/uploads/source/news/2022/opinion/shutterstock1914164533-1652959735.jpg&w=900&height=601"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/screen/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../component/header/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <div className="container CourseCardHome"></div>
+    </div>
+  );
+};
+
+export default Home;
